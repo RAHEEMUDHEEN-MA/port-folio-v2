41ae472560c595ae6764f5be1e4d70e5c9acc9bd
RAHEEMUDHEEN-MA <raheemudheenma118@gmail.com>
2026-09-20 15:39:02 +0530

    feat: updated resume gdrive link

--- src/js/config/constants.js
+++ src/js/config/constants.js
@@ -5,5 +5,5 @@ export const CONSTANTS = {
     EMAIL: 'raheemudheen.ma@gmail.com',
     GITHUB_URL: 'https://github.com/raheemudheen-ma',
     LINKEDIN_URL: 'https://linkedin.com/in/raheemudheen-ma',
-    RESUME_URL: 'https://drive.google.com/file/d/1HFU1vArEgjjQ-5fuFnRYXTItDt9qdTM3/view?usp=drive_link'
+    RESUME_URL: 'https://drive.google.com/file/d/1SAn8TyehXZFgNmIdSxB5056XSP6Yl6h3/view?usp=drive_link'
 };
